f8f25d28a807a2652f7dfe41fa64c027a8e6991c
bellybelly21 <sybellasss@gmail.com>
2026-08-05 22:56:21 -0400

    Deploy para vercel con corrección de error (faltaba exportar algo válido en 2 páginas)

--- src/app/proyectos/page.tsx
+++ src/app/proyectos/page.tsx
@@ -4,3 +4,11 @@ export const metadata: Metadata = {
     title: "Proyectos y trabajos destacados", 
     description: "Explora mis proyectos recientes en desarrollo frontend y diseño de interfaces. Soluciones digitales escalables construidas con React, TypeScript y buenas prácticas.",
   };
+
+  export default function ProyectosPage() {
+    return (
+      <main>
+        {/* Contenido del sitio que aún no me digno a maquetar*/}
+      </main>
+    );
+  }
--- src/app/sobre-mi/page.tsx
+++ src/app/sobre-mi/page.tsx
@@ -4,3 +4,12 @@ export const metadata: Metadata = {
     title: "Sobre Mí | Experiencia y Trayectoria", 
     description: "Conoce mi trayectoria como diseñadora UX/UI y frontend developer. Mis gustos personales, habilidades y proyecciones.",
   };
+
+  
+  export default function SobreMiPage() {
+    return (
+      <main>
+        {/* Contenido del sitio que aún no me digno a maquetar*/}
+      </main>
+    );
+  }
